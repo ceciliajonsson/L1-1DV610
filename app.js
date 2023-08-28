db26b1de74d3bddc3b6060d3104f0b6d06694158
Cecilia Jonsson <cj222mz@student.lnu.se>
2023-08-28 23:46:50 +0200

    Generate a new color

--- app.js
+++ app.js
@@ -1,33 +1,45 @@
 
-
-const inputName = document.getElementById('input-name');
-const submitButton = document.getElementById('submit-button');
-const welcomeMessage = document.getElementById('welcome-message');
 const userInput = document.getElementById('user-input')
+const inputName = document.getElementById('input-name')
+const submitButton = document.getElementById('submit-button')
+const userMessage = document.getElementById('user-message')
+const welcomeMessage = document.getElementById('welcome-message')
+const colorMessage = document.getElementById('color-message')
+const changeColorButton = document.getElementById('change-color-button')
 
-// Show name without using innerHTML
 submitButton.addEventListener('click', () => {
     if (inputName.value === '') {
-        alert('Please enter your name');
+        alert('Please enter your name')
         return;
     }
-    welcomeMessage.textContent = `Welcome ${inputName.value}`;
-    welcomeMessage.style.display = 'block';
-    userInput.style.display = 'none';
+    userInput.style.display = 'none'
+    userMessage.style.display = 'block'
+    welcomeMessage.textContent = `Welcome ${inputName.value}.`
+    colorMessage.textContent =`The random color generated for you is ${randomColor()}.`
+
     changeBackgroundColor()
 
 });
+
+changeColorButton.addEventListener('click', () => {
+
+    changeBackgroundColor()
+            colorMessage.textContent =`The random color generated for you is ${randomColor()}.`
+
+});
 function changeBackgroundColor() {
-    document.body.style.backgroundColor = randomColor();
+    const color = randomColor()
+    document.body.style.backgroundColor = color
+    return color; 
 }
 
 function randomColor() {
-    var red = getRandomInt(0, 255);
-    var green = getRandomInt(0, 255);
-    var blue = getRandomInt(0, 255);
-    return "rgb(" + red + "," + green + "," + blue + ")";
+    var red = getRandomInt(0, 255)
+    var green = getRandomInt(0, 255)
+    var blue = getRandomInt(0, 255)
+    return "rgb(" + red + "," + green + "," + blue + ")"
 }
 function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+    return Math.floor(Math.random() * (max - min + 1)) + min
 }
 
