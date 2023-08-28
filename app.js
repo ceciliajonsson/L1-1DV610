246af2b6e302d4b9330711cbd3a6f8b70c2875fb
Cecilia Jonsson <cj222mz@student.lnu.se>
2023-08-28 22:18:15 +0200

    Add simple functonality (hide/show & alert)

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const inputName = document.getElementById('input-name');
 const submitButton = document.getElementById('submit-button');
 const welcomeMessage = document.getElementById('welcome-message');
+const userInput = document.getElementById('user-input')
 
 // Show name without using innerHTML
 submitButton.addEventListener('click', () => {
+    if (inputName.value === '') {
+        alert('Please enter your name');
+        return;
+    }
     welcomeMessage.textContent = `Welcome ${inputName.value}`;
+    welcomeMessage.style.display = 'block';
+    userInput.style.display = 'none';
 });
