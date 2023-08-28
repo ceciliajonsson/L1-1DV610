0c737fc3d72493fdebdb3c962e942248b1178cad
Cecilia Jonsson <cj222mz@student.lnu.se>
2023-08-28 23:11:30 +0200

    Generate random background color

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+
+
 const inputName = document.getElementById('input-name');
 const submitButton = document.getElementById('submit-button');
 const welcomeMessage = document.getElementById('welcome-message');
@@ -12,4 +14,20 @@ submitButton.addEventListener('click', () => {
     welcomeMessage.textContent = `Welcome ${inputName.value}`;
     welcomeMessage.style.display = 'block';
     userInput.style.display = 'none';
+    changeBackgroundColor()
+
 });
+function changeBackgroundColor() {
+    document.body.style.backgroundColor = randomColor();
+}
+
+function randomColor() {
+    var red = getRandomInt(0, 255);
+    var green = getRandomInt(0, 255);
+    var blue = getRandomInt(0, 255);
+    return "rgb(" + red + "," + green + "," + blue + ")";
+}
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
