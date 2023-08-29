9d03c7fd18c66c9f68265bfa6d2169ce0e386aa1
Cecilia Jonsson <cj222mz@student.lnu.se>
2023-08-29 20:35:22 +0200

    Add comments

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-
+// Get DOM elements by their IDs
 const userInput = document.getElementById('user-input')
 const inputName = document.getElementById('input-name')
 const submitButton = document.getElementById('submit-button')
@@ -9,13 +9,16 @@ const changeColorButton = document.getElementById('change-color-button')
 const changeNameButton = document.getElementById('change-name-button')
 
 
+// Event listener for the submit button
 submitButton.addEventListener('click', () => {
+    // If the name input field is empty, alert the user to enter their name
     if (inputName.value === '') {
         alert('Please enter your name')
         return
     }
     userInput.style.display = 'none'
     userMessage.style.display = 'block'
+    
     welcomeMessage.textContent = `Welcome ${inputName.value}!`
     colorMessage.textContent =`The random color generated for you is ${randomColor()}.`
 
@@ -23,31 +26,37 @@ submitButton.addEventListener('click', () => {
 
 })
 
-
+// Event listener for the change name button
 changeNameButton.addEventListener('click', () => {
     userInput.style.display = 'block'
     userMessage.style.display = 'none'
     inputName.value = ''
 })
 
+// Event listener for the change color button
 changeColorButton.addEventListener('click', () => {
 
     changeBackgroundColor()
             colorMessage.textContent =`The random color generated for you is ${randomColor()}.`
 
 })
+
+// Function to change the background color
 function changeBackgroundColor() {
     const color = randomColor()
     document.body.style.backgroundColor = color
     return color;
 }
 
+// Function to generate a random color
 function randomColor() {
     var red = getRandomInt(0, 255)
     var green = getRandomInt(0, 255)
     var blue = getRandomInt(0, 255)
     return "rgb(" + red + "," + green + "," + blue + ")"
 }
+
+// Function to generate a random number
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min
 }
